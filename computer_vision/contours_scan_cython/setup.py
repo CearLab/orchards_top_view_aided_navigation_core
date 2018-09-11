931b68808232d80bbc7f781d79461d7e30478de6
Omer Shalev <omershalva@gmail.com>
2018-09-11 13:31:23 +0300

    Removed commented out lines

--- computer_vision/contours_scan_cython/setup.py
+++ computer_vision/contours_scan_cython/setup.py
@@ -1,11 +1,3 @@
-# from distutils.core import setup
-# from Cython.Build import cythonize
-#
-# setup(
-#     ext_modules = cythonize("contours_scan.pyx")
-# )
-
-
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
