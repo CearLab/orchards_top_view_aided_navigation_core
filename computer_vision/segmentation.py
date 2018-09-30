0c8b97ed4f7a6579392a8b8c4631fb78300b4d13
Omer Shalev <omershalva@gmail.com>
2018-09-30 17:48:26 +0300

    Removed irrelevant TODOs

--- computer_vision/segmentation.py
+++ computer_vision/segmentation.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from framework import cv_utils
 
-def extract_canopy_contours(image, lower_color=None, upper_color=None, min_area=None):  # TODO: generalize min_area (should be perhaps top N contours?)
+def extract_canopy_contours(image, lower_color=None, upper_color=None, min_area=None, margin_width=0, margin_color=None):  # TODO: generalize min_area (should be perhaps top N contours?)
     if lower_color is None and upper_color is None:
         green_lower_hue_degrees = 65
         green_lower_saturation_percent = 5
@@ -15,23 +15,19 @@ def extract_canopy_contours(image, lower_color=None, upper_color=None, min_area=
         upper_color = np.array([green_upper_hue_degrees / 2.0, green_upper_saturation_percent * 255.0 / 100, green_upper_value_percent * 255.0 / 100])
     if min_area is None:
         min_area = 10000
-
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     hsv_mask = cv2.inRange(hsv_image, lower_color, upper_color)
     _, contours, hierarchy = cv2.findContours(hsv_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     contours = filter(lambda contour: cv2.contourArea(contour) > min_area, contours)
-    contours_mask = cv2.cvtColor(cv2.drawContours(np.zeros(image.shape, np.uint8), contours, contourIdx=-1, color=(255,255,255), thickness=-1),
-                                 cv2.COLOR_RGB2GRAY)
+    contours_mask = cv2.drawContours(np.zeros((image.shape[0], image.shape[1]), np.uint8), contours, contourIdx=-1, color=255, thickness=-1)
+    if margin_width != 0: # TODO: consider moving this part to the calling functions (map generation)
+        contours_mask = cv2.drawContours(np.zeros((image.shape[0], image.shape[1]), np.uint8), contours, contourIdx=-1, color=margin_color, thickness=margin_width * 2)
+        contours_mask = cv2.drawContours(contours_mask, contours, contourIdx=-1, color=255, thickness=-1)
     return contours, contours_mask
-
-    # TODO: consider: erode-dialate
 
 
 def extract_vehicle(image, roi_center_x=None, roi_center_y=None, roi_size_x=None, roi_size_y=None,
                     lower_color=None, upper_color=None):
-    # TODO: fine tune logic!
-    # TODO: look only where the previous contour was (if not found or jumped to much - return None)
-    # TODO: apply kalman filter for smoothing
     if lower_color is None and upper_color is None:
         purple_lower_hue_degrees = 210
         purple_lower_saturation_percent = 60
@@ -47,6 +43,8 @@ def extract_vehicle(image, roi_center_x=None, roi_center_y=None, roi_size_x=None
     hsv_mask = cv2.inRange(hsv_image, lower_color, upper_color)
     hsv_mask = cv2.dilate(hsv_mask, kernel=np.ones((9,9),np.uint8), iterations=1)
     _, contours, hierarchy = cv2.findContours(hsv_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    if len(contours) == 0:
+        return None
     largest_contour = sorted(contours, key=lambda contour: cv2.contourArea(contour), reverse=True)[0]
     moments = cv2.moments(largest_contour)
     if roi_center_x is not None and roi_center_y is not None:
@@ -56,21 +54,3 @@ def extract_vehicle(image, roi_center_x=None, roi_center_y=None, roi_size_x=None
         contour_center_x = int(moments['m10'] / moments['m00'])
         contour_center_y = int(moments['m01'] / moments['m00'])
     return contour_center_x, contour_center_y
-
-
-
-def extract_canopies_map(image, roi_center_x=None, roi_center_y=None, roi_size_x=None, roi_size_y=None,
-                         lower_color=None, upper_color=None, min_area=None):
-    contours_map = np.full((np.size(image, 0), np.size(image, 1)), fill_value=0, dtype=np.uint8)
-    if roi_center_x is not None and roi_center_y is not None:
-        roi_image, roi_upper_left, roi_lower_right = cv_utils.crop_region(image, roi_center_x, roi_center_y, roi_size_x, roi_size_y)
-        contours, _ = extract_canopy_contours(roi_image, lower_color, upper_color, min_area)
-        roi_contours_map = np.full((roi_image.shape[0], roi_image.shape[1]), fill_value=0, dtype=np.uint8)
-        cv2.drawContours(roi_contours_map, contours, contourIdx=-1, color=128, thickness=-1)
-        cv2.drawContours(roi_contours_map, contours, contourIdx=-1, color=255, thickness=3)
-        contours_map = cv_utils.insert_image_patch(contours_map, roi_contours_map, roi_upper_left, roi_lower_right)
-    else:
-        contours, _ = extract_canopy_contours(image, lower_color, upper_color, min_area)
-        cv2.drawContours(contours_map, contours, contourIdx=-1, color=128, thickness=-1)
-        cv2.drawContours(contours_map, contours, contourIdx=-1, color=255, thickness=3)
-    return contours_map
