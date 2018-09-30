93f0fcb928708d9adb029cb7ea390c4794220dd9
Omer Shalev <omershalva@gmail.com>
2018-09-30 17:47:11 +0300

    added calculation of pixel to meter ratio

--- computer_vision/calibration.py
+++ computer_vision/calibration.py
@@ -1,11 +1,14 @@
 import numpy as np
 
-def calculate_average_trunk_radius_in_pixels(row_width_in_pixels, intra_row_distance_in_pixels, measured_row_widths_in_meters,
-                                             measured_intra_row_distances_in_meters, measured_trunk_perimeters_in_meters, trunk_dilation_ratio):
-    pixel_to_meter_x_ratio = row_width_in_pixels / np.mean(measured_row_widths_in_meters) # TODO: verify
-    pixel_to_meter_y_ratio = intra_row_distance_in_pixels / np.mean(measured_intra_row_distances_in_meters) # TODO: verify
-    mean_pixel_to_meter_ratio = np.mean([pixel_to_meter_x_ratio, pixel_to_meter_y_ratio])
-    mean_trunk_perimeter_in_meters = np.mean(measured_trunk_perimeters_in_meters)
-    mean_trunk_radius_in_meters = mean_trunk_perimeter_in_meters / (2 * np.pi)
-    trunk_radius = int(np.round(mean_trunk_radius_in_meters * mean_pixel_to_meter_ratio * trunk_dilation_ratio))
-    return trunk_radius
+def calculate_trunk_radius_in_meters(measured_trunk_perimeters_in_meters):
+    approximated_trunk_radii_in_meters = map(lambda perimeter: perimeter / (2 * np.pi), measured_trunk_perimeters_in_meters)
+    mean_trunk_radius_in_meters = np.mean(approximated_trunk_radii_in_meters)
+    std_trunk_radius_in_meters = np.std(approximated_trunk_radii_in_meters)
+    return mean_trunk_radius_in_meters, std_trunk_radius_in_meters
+
+
+def calculate_pixel_to_meter_ratio(grid_dim_x_in_pixels, grid_dim_y_in_pixels, measured_row_widths_in_meters, measured_intra_row_distances_in_meters):
+    mean_row_width_in_meters = np.mean(measured_row_widths_in_meters)
+    mean_intra_row_distance_in_meters = np.mean(measured_intra_row_distances_in_meters)
+    return float(np.mean([float(grid_dim_x_in_pixels) / float(mean_row_width_in_meters), float(grid_dim_y_in_pixels) / float(mean_intra_row_distance_in_meters)]))
+    # TODO: for this calculation to be valid, you need to show that camera's Fx ~ Fy (calibration output)
