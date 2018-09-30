09cace0864406ef3953a6ba688a7fac35516f7e6
Omer Shalev <omershalva@gmail.com>
2018-09-30 20:25:12 +0300

    Renamed function

--- computer_vision/calibration.py
+++ computer_vision/calibration.py
@@ -7,7 +7,7 @@ def calculate_trunk_radius_in_meters(measured_trunk_perimeters_in_meters):
     return mean_trunk_radius_in_meters, std_trunk_radius_in_meters
 
 
-def calculate_pixel_to_meter_ratio(grid_dim_x_in_pixels, grid_dim_y_in_pixels, measured_row_widths_in_meters, measured_intra_row_distances_in_meters):
+def calculate_pixel_to_meter(grid_dim_x_in_pixels, grid_dim_y_in_pixels, measured_row_widths_in_meters, measured_intra_row_distances_in_meters):
     mean_row_width_in_meters = np.mean(measured_row_widths_in_meters)
     mean_intra_row_distance_in_meters = np.mean(measured_intra_row_distances_in_meters)
     return float(np.mean([float(grid_dim_x_in_pixels) / float(mean_row_width_in_meters), float(grid_dim_y_in_pixels) / float(mean_intra_row_distance_in_meters)]))
